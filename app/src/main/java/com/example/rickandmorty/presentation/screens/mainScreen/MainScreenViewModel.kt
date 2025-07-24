a4eacd3c6a2db6f9c83cc21faf97fa7a6ee9f932
Pave1iOS <PGribachev94@yandex.ru>
2025-07-25 00:02:01 +0300

    + logs

--- app/src/main/java/com/example/rickandmorty/presentation/screens/mainScreen/MainScreenViewModel.kt
+++ app/src/main/java/com/example/rickandmorty/presentation/screens/mainScreen/MainScreenViewModel.kt
@@ -1,9 +1,11 @@
 package com.example.rickandmorty.presentation.screens.mainScreen
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.rickandmorty.data.api.Character
 import com.example.rickandmorty.domain.NetworkRepository
+import com.example.rickandmorty.utils.LogSource
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
@@ -26,15 +28,25 @@ class MainScreenViewModel @Inject constructor(
         viewModelScope.launch {
             _isLoading.value = true
             _errorMessage.value = null
+            Log.i(TAG, "${LogSource.VIEWMODEL} start load \n" +
+                    "_isLoading = ${_isLoading.value}")
 
             try {
                 val result = repository.getListCharacters().getOrThrow()
                 _characters.value = result
+                Log.i(TAG, "${LogSource.VIEWMODEL} getAllCharacters = ${_characters.value}")
             } catch (e: Throwable) {
                 _errorMessage.value = "Ошибка загрузки данных: ${e.localizedMessage}"
+                Log.e(TAG, "${LogSource.VIEWMODEL} ${e.message}")
             } finally {
                 _isLoading.value = false
+                Log.i(TAG, "${LogSource.VIEWMODEL} finish load \n" +
+                        "_isLoading = ${_isLoading.value}")
             }
         }
+    }
+
+    companion object {
+        private const val TAG = "MainScreenViewModel"
     }
 }
