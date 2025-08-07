a652f09bb72ec446db6b231eed032be365a8c831
Pave1iOS <PGribachev94@yandex.ru>
2025-08-07 15:48:21 +0300

    + database worked

---
+++ app/src/main/java/com/example/rickandmorty/domain/AppDatabase.kt
@@ -0,0 +1,7 @@
+package com.example.rickandmorty.domain
+
+import com.example.rickandmorty.data.api.RickAndMortyCharacter
+
+@Database(entities = [RickAndMortyCharacter::class], version = 1)
+class AppDatabase {
+}
